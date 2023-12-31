b03203784770189ad88e93dee53f260ee24af240
ILMALUTFHY <ilmalutfhy@gmail.com>
2023-12-31 15:58:54 +0700

    modified:   anggota/index.php

--- arisan/index.php
+++ arisan/index.php
@@ -11,7 +11,7 @@
 <!-- <div class="container"> -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
   <div class="container ">
-    <a class="navbar-brand" href="#">Arisan Online KITA</a>
+    <a class="navbar-brand" href="#">Arisan Online</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
